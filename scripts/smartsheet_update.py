a1850f0609be2f10fd9846d10cf420d6b791e6df
Sage <sage.gendron@gmail.com>
2022-11-11 13:49:25 -0500

    smartsheet_update reorganization & abstraction

--- scripts/smartsheet_update.py
+++ scripts/smartsheet_update.py
@@ -74,7 +74,7 @@ def create_cell(ss_c, col_name, wb=None, sheet=None, xl_cell=None, is_float=Fals
     :param str sheet: Sheet name to reference when extracting from wb (xlwings workbook)
     :param str xl_cell: Cell reference from which to extract data within the sheet, book above
     :param bool is_float: indicates if value should be read as a float
-    :param bool is_bool: indicates if values should be read as a bool
+    :param bool is_bool: indicates if value should be read as a bool
     :param cell_val: cell value, if known/is a stock value, so it can be attributed to the new cell object
     :returns new_cell: a new Cell object with value as specified
     :rtype: smartsheet.Smartsheet.models.Cell
@@ -99,23 +99,122 @@ def create_cell(ss_c, col_name, wb=None, sheet=None, xl_cell=None, is_float=Fals
     return new_cell
 
 
+def upload_attachments(ss_c, row_id, quote, schedule, submittal, opt_quote):
+    """
+    www
+
+    :param ss_c:
+    :param row_id:
+    :param quote:
+    :param schedule:
+    :param submittal:
+    :param opt_quote:
+    :return:
+    :rtype: list
+    """
+    _updated_attachments = []
+    update_quote_upload = False
+    update_schedule_upload = False
+    update_submittal_upload = False
+    update_opt_quote_upload = False
+
+    # grab existing attachments (if they exist) to help decide what to update
+    current_attachments = ss_c.Attachments.list_row_attachments(sheet_id, row_id)
+
+    # loop through row attachments to identify if updates are required based on file name
+    attachment: smartsheet.Smartsheet.models.Attachment
+    for attachment in current_attachments.data:
+        if attachment.name == quote.split('\\')[-1]:
+            update_quote_upload = True
+        elif attachment.name == schedule.split('\\')[-1]:
+            update_schedule_upload = True
+        elif attachment.name == submittal.split('\\')[-1]:
+            update_submittal_upload = True
+        elif attachment.name == opt_quote.split('\\')[-1]:
+            update_opt_quote_upload = True
+
+    # attaches a new version of the pdf quote if one was found, otherwise uploads a new file
+    if update_quote_upload:
+        _updated_attachments.append(ss_c.Attachments.attach_new_version(sheet_id, update_quote_upload, (
+            quote.split('\\')[-1], open(quote, 'rb'), 'application/pdf')))
+    else:
+        _updated_attachments.append(ss_c.Attachments.attach_file_to_row(sheet_id, row_id, (
+            quote.split('\\')[-1], open(quote, 'rb'), 'application/pdf')))
+
+    # attaches a new version of the engineered schedule if one was found, otherwise uploads a new file
+    if update_schedule_upload:
+        _updated_attachments.append(ss_c.Attachments.attach_new_version(
+            sheet_id, update_schedule_upload, (schedule.split('\\')[-1], open(schedule, 'rb'),
+                                               'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')))
+    else:
+        _updated_attachments.append(ss_c.Attachments.attach_file_to_row(
+            sheet_id, row_id, (schedule.split('\\')[-1], open(schedule, 'rb'),
+                               'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')))
+
+    # attaches a new version of the submittal if one was found, otherwise attaches a new file
+    try:
+        if update_submittal_upload:
+            _updated_attachments.append(ss_c.Attachments.attach_new_version(sheet_id, update_submittal_upload, (
+                submittal.split('\\')[-1], open(submittal, 'rb'), 'application/pdf')))
+        else:
+            _updated_attachments.append(ss_c.Attachments.attach_file_to_row(sheet_id, row_id, (
+                submittal.split('\\')[-1], open(submittal, 'rb'), 'application/pdf')))
+    # since this file doesn't exist for every quote produced, pass on error
+    except FileNotFoundError:
+        pass
+
+    # attaches a new version of the Excel customer quote if one was found, otherwise uploads a new file
+    try:
+        if update_opt_quote_upload:
+            _updated_attachments.append(ss_c.Attachments.attach_new_version(
+                sheet_id, update_opt_quote_upload, (opt_quote.split('\\')[-1], open(opt_quote, 'rb'),
+                                                    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')))
+        else:
+            _updated_attachments.append(ss_c.Attachments.attach_file_to_row(
+                sheet_id, row_id, (opt_quote.split('\\')[-1], open(opt_quote, 'rb'),
+                                   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')))
+    # since this file doesn't exist for every quote produced, pass on error
+    except FileNotFoundError:
+        pass
+
+    return _updated_attachments
+
+
+def upload_discussions(ss_c, row_id, fpath):
+    """
+
+    :param ss_c:
+    :param row_id:
+    :param fpath:
+    :return:
+    """
+    # build comment object with filepath for text
+    new_comment: smartsheet.Smartsheet.models.Comment = ss_c.models.Comment({'text': fpath})
+
+    # grab existing discussions (if they exist) to help decide what to update
+    current_discussions = ss_c.Discussions.get_row_discussions(sheet_id, row_id, include_all=True)
+
+    discussion: smartsheet.Smartsheet.models.Discussion
+    for discussion in current_discussions.data:
+        for comment in discussion.comments:
+            if comment.text == fpath:
+                return ss_c.Discussions.update_comment(sheet_id, comment.id, new_comment)
+
+    # add/update filepath to the project folder as a row comment
+    return ss_c.Discussions.create_discussion_on_row(
+        sheet_id, row_id, ss_c.models.Discussion({'comment': new_comment}))
+
+
 def update_ss():
     """
-    Updates the Quotes Smartsheet with information from the calling project file as detailed by created cells in this
-    function. Also checks for existing attachments and discussions to update in lieu of create (where applicable).
+    Updates the primary Estimating Smartsheet with information from the calling Excel file as detailed by created
+    cells in this function. Also checks for existing attachments and discussions to update in lieu of create
+    (where applicable).
 
     :return: None
     """
     # instantiate Book instance to interact with Excel
     wb: xw.Book = xw.Book.caller()
-
-    fpath = '\\'.join(wb.fullname.split('\\')[:-1])
-
-    quote_name = rename(wb, 'QUOTE', 'pdf')
-    opt_quote_name = rename(wb, 'EXCEL QUOTE', 'xlsx')
-    schedule_name = rename(wb, 'SCHEDULE', 'xlsx')
-    submittal_name = rename(wb, 'SUBMITTAL', 'pdf')
-
     # instantiate smartsheet client with Quotes smartsheet
     ss_c: smartsheet.Smartsheet = get_ss_client(wb)
 
@@ -123,37 +222,6 @@ def update_ss():
     job_row_id: int = int(wb.sheets['SCHEDULE'].range(sch_row_id_cell).value)
     job_row: smartsheet.Smartsheet.models.Row = ss_c.models.Row()
     job_row.id = job_row_id
-
-    # build comment object with filepath for text
-    fpath_comment: smartsheet.Smartsheet.models.Comment = ss_c.models.Comment({'text': fpath})
-
-    # instantiate flags to skip update calls below
-    update_fpath_comment = None
-    update_quote_upload = None
-    update_schedule_upload = None
-    update_submittal_upload = None
-    update_opt_quote_upload = None
-
-    # grab existing attachments, discussions to help decide what to update
-    current_attachments = ss_c.Attachments.list_row_attachments(sheet_id, job_row_id)
-    current_discussions = ss_c.Discussions.get_row_discussions(sheet_id, job_row_id, include_all=True)
-
-    discussion: smartsheet.Smartsheet.models.Discussion
-    for discussion in current_discussions.data:
-        for comment in discussion.comments:
-            if comment.text == fpath:
-                update_fpath_comment = comment.id
-
-    attachment: smartsheet.Smartsheet.models.Attachment
-    for attachment in current_attachments.data:
-        if attachment.name == quote_name.split('\\')[-1]:
-            update_quote_upload = attachment.id
-        elif attachment.name == schedule_name.split('\\')[-1]:
-            update_schedule_upload = attachment.id
-        elif attachment.name == submittal_name.split('\\')[-1]:
-            update_submittal_upload = attachment.id
-        elif attachment.name == opt_quote_name.split('\\')[-1]:
-            update_opt_quote_upload = attachment.id
 
     # create SS cell instance, assign deal total from quote, add cell to SS row object
     job_row.cells.append(create_cell(ss_c, 'Quote Value', xl_cell=qte_total_cell, wb=wb, sheet='QUOTE', is_float=True))
@@ -165,75 +233,35 @@ def update_ss():
     col: str
     xl_cell: str
     for col, xl_cell in simple_cols.items():
-        # create Cell instance, assign quote status to Completed, add cell to SS row object
+        # create Cell instance, assign quote status to Completed, add cell to Row object
         if col == 'Status':
             job_row.cells.append(create_cell(ss_c, col, cell_val='Completed'))
-        # create Cell instance, assign boolean value, add cell to SS row object
+        # create Cell instance, assign boolean value, add cell to Row object
         elif col == 'Special Case 1':
             job_row.cells.append(create_cell(ss_c, 'Special Case 1', wb=wb, sheet='SCHEDULE', xl_cell=xl_cell,
                                              is_bool=True))
-        # create Cell instance, assign customer pricing from quote, add cell to SS row object
+        # create Cell instance, assign customer pricing from quote, add cell to Row object
         elif col == 'Customer Pricing':
             job_row.cells.append(
                 create_cell(ss_c, col, wb=wb, xl_cell=xl_cell, sheet='QUOTE', is_float=True))
         elif type(cell_val := wb.sheets['SCHEDULE'].range(xl_cell).value) is str:
             job_row.cells.append(create_cell(ss_c, col, cell_val=cell_val))
 
-    # push row update to SS server
+    # push row update to Smartsheet server
     _updated_row = ss_c.Sheets.update_rows(sheet_id, [job_row])
 
-    # add/update filepath to the project folder as comment
-    if update_fpath_comment is not None:
-        _updated_comment = ss_c.Discussions.update_comment(sheet_id, update_fpath_comment, fpath_comment)
-    else:
-        _updated_comment = ss_c.Discussions.create_discussion_on_row(
-            sheet_id, job_row_id, ss_c.models.Discussion({'comment': fpath_comment}))
-
-    # initiate file attachment upload to row by filename/path
-    if update_quote_upload is not None:
-        _upload_quote = ss_c.Attachments.attach_new_version(sheet_id, update_quote_upload, (
-            quote_name.split('\\')[-1], open(quote_name, 'rb'), 'application/pdf'))
-    else:
-        _upload_quote = ss_c.Attachments.attach_file_to_row(sheet_id, job_row_id, (quote_name.split('\\')[-1],
-                                                            open(quote_name, 'rb'), 'application/pdf'))
-
     #
-    try:
-        if update_submittal_upload is not None:
-            _upload_submittal = ss_c.Attachments.attach_new_version(sheet_id, update_submittal_upload, (
-                submittal_name.split('\\')[-1], open(submittal_name, 'rb'), 'application/pdf'))
-        else:
-            _upload_submittal = ss_c.Attachments.attach_file_to_row(sheet_id, job_row_id, (
-                submittal_name.split('\\')[-1], open(submittal_name, 'rb'), 'application/pdf'))
-    except FileNotFoundError:
-        pass
+    quote = rename(wb, 'QUOTE', 'pdf')
+    schedule = rename(wb, 'SCHEDULE', 'xlsx')
+    submittal = rename(wb, 'SUBMITTAL', 'pdf')
+    opt_quote = rename(wb, 'EXCEL QUOTE', 'xlsx')
+    # push attachments to Smartsheet server
+    _updated_attachments = upload_attachments(ss_c, job_row_id, quote, schedule, submittal, opt_quote)
 
-    #
-    if update_schedule_upload is not None:
-        _upload_schedule = ss_c.Attachments.attach_new_version(
-            sheet_id, update_schedule_upload, (schedule_name.split('\\')[-1], open(schedule_name, 'rb'),
-                                               'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'))
-    else:
-        _upload_schedule = ss_c.Attachments.attach_file_to_row(
-            sheet_id, job_row_id, (schedule_name.split('\\')[-1], open(schedule_name, 'rb'),
-                                   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'))
-
-    #
-    if update_opt_quote_upload is not None:
-        try:
-            _upload_opt_quote = ss_c.Attachments.attach_new_version(
-                sheet_id, update_opt_quote_upload, (opt_quote_name.split('\\')[-1], open(opt_quote_name, 'rb'),
-                                                    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'))
-        except FileNotFoundError:
-            pass
-
-    else:
-        try:
-            _upload_opt_quote = ss_c.Attachments.attach_file_to_row(
-                sheet_id, job_row_id, (opt_quote_name.split('\\')[-1], open(opt_quote_name, 'rb'),
-                                       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'))
-        except FileNotFoundError:
-            pass
+    # identify filepath for entry as a comment for reference by sales reps
+    fpath = '\\'.join(wb.fullname.split('\\')[:-1])
+    # push discussions to Smartsheet server
+    _updated_discussions = upload_discussions(ss_c, fpath, job_row_id)
 
 
 def mark_as_won():
