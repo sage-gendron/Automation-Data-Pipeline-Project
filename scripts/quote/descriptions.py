fec3fc02f8b16b3c18bf58bcd8a8162801e3a570
Sage <sage.gendron@gmail.com>
2022-11-29 10:08:35 -0500

    rework completed

--- scripts/quote/descriptions.py
+++ scripts/quote/descriptions.py
@@ -1,4 +1,4 @@
-# descriptions.py
+# scripts/quote/descriptions.py
 """
 author: Sage Gendron
 Creates descriptions based on information provided on the engineered schedule. Only generates one description per
@@ -6,6 +6,7 @@ package key.
 Assigns descriptions to the to-be-exported data packet and, on a different call, assigns descriptions to the quote file
 by package key.
 """
+# maps available description cells by package key
 quote_kit_descrip: dict[str, str] = {
     'A': 'H15', 'B': 'H30', 'C': 'H45', 'D': 'H60', 'E': 'H75', 'F': 'H90', 'G': 'H105', 'H': 'H120', 'I': 'H135',
     'J': 'H150', 'K': 'H165', 'L': 'H180', 'M': 'H195', 'N': 'H210', 'O': 'H225', 'P': 'H240', 'Q': 'H255', 'R': 'H270',
@@ -16,7 +17,7 @@ quote_kit_descrip: dict[str, str] = {
 
 def generate_description(json_sch):
     """
-    Creates description per package based on size, coil, and cv (if info supplied).
+    Creates description per package based on system size, connection, and control type/size (if info supplied).
 
     :param list json_sch: list of dictionaries representing the Excel engineered schedule
     :return:
@@ -28,15 +29,15 @@ def generate_description(json_sch):
     pack_eq_types: dict[str, list[str]] = {}
     skip_eq: list[str] = []
 
-    # iterate over json entries with iterative variable 'kit'
-    kit: int
-    for kit in range(len(json_sch)):
+    # iterate over JSON entries
+    row: dict[str, ...]
+    for row in json_sch:
         # pull out package key to check if description has already been created
-        pkg: str = json_sch[kit]['pkg_key']
+        pkg: str = row['pkg_key']
 
-        # if the pkg key is in descriptions, the description has already been generated, so it just checks eq_type
+        # if the pkg key is in descriptions, the description has already been generated, so just check eq_type
         if pkg in descriptions.keys():
-            if pkg not in skip_eq and (eq_type := json_sch[kit]['eq_type']) not in pack_eq_types[pkg]:
+            if pkg not in skip_eq and (eq_type := row['eq_type']) not in pack_eq_types[pkg]:
                 pack_eq_types[pkg].append(eq_type)
 
         # if the pkg key is not in the dict it will form a list that will be referred to in assign_descrip_to_quote
@@ -47,17 +48,17 @@ def generate_description(json_sch):
             inc_control: bool = False
 
             # create eq_type list in dictionary to be appended to descriptions later
-            if type(eq_type := json_sch[kit]['eq_type']) not in (None, float):
+            if type(eq_type := row['eq_type']) not in (None, float):
                 pack_eq_types[pkg] = [eq_type]
             elif pkg not in skip_eq:
                 skip_eq.append(pkg)
 
-            # grab relevant fields from schedule row to speed up lookups
-            rate: float = json_sch[kit]['rate']
-            size: str = json_sch[kit]['size']
-            conn_size: str = json_sch[kit]['conn_size']
-            control_size_type: str = json_sch[kit]['control_size_type']
-            control_type: str = json_sch[kit]['control_type']
+            # grab relevant fields from schedule row
+            rate: float = row['rate']
+            size: str = row['size']
+            conn_size: str = row['conn_size']
+            control_size_type: str = row['control_size_type']
+            control_type: str = row['control_type']
 
             # check for small size kits
             is_sm: bool = True if size == 'SMALL' and rate <= 5 else False
@@ -66,7 +67,7 @@ def generate_description(json_sch):
             control_method: str
             if type(control_type) not in (None, float):
                 # if control type 2, make control_method CTRL_TYPE_2 else grab control_method from schedule
-                control_method = 'CTRL_TYPE_2' if 'CTRL_TYPE_2' in control_type else json_sch[kit]['control_method']
+                control_method = 'CTRL_TYPE_2' if 'CTRL_TYPE_2' in control_type else row['control_method']
             else:
                 # default to not including any of the below 3 items
                 control_method = ''
@@ -84,7 +85,7 @@ def generate_description(json_sch):
             if size != control_size and control_size != 'TBD':
                 inc_control = True
 
-            # start with system size
+            # start building the description with system size
             description.append(size)
 
             # if small size package, add the word before control type
@@ -110,10 +111,10 @@ def generate_description(json_sch):
             descriptions[pkg] = ' '.join(description)
 
     complete_descrips: dict[str, str] = {}
-    kit: int
-    for kit in range(len(json_sch)):
+    row: dict[str, ...]
+    for row in json_sch:
         # assign description to json_sch for export in data packet
-        pkg: str = json_sch[kit]['pkg_key']
+        pkg: str = row['pkg_key']
         if pkg not in complete_descrips.keys():
             if pkg not in skip_eq:
                 # compile equipment types per package into a single string
@@ -122,7 +123,7 @@ def generate_description(json_sch):
                 complete_descrips[pkg] = f"{descriptions[pkg]} ({eq_types})"
             else:
                 complete_descrips[pkg] = descriptions[pkg]
-        json_sch[kit]['quote_descrip'] = complete_descrips[pkg]
+        row['quote_descrip'] = complete_descrips[pkg]
 
     return json_sch, complete_descrips
 
@@ -133,7 +134,7 @@ def assign_descrip_to_quote(wb, descrips_by_pkg):
     **This function is non-dynamic and iterates through quote_kit_descrip global variable at top of file**
 
     :param xw.Book wb: represents combination schedule/quote file
-    :param dict descrips_by_pkg: dict of package key: description
+    :param dict descrips_by_pkg: dict of {package key: description}
     :return: None - changes made directly to Excel file
     """
     # iterate through generated descriptions and assign package descriptions to quote sheet
--- scripts/quote/kit_types.py
+++ scripts/quote/kit_types.py
@@ -1,55 +1,56 @@
-# kit_types.py
-from connections import typ_system_type, ctrl_conn_type, supply_conn_type, return_conn_type
-
+# scripts/quote/kit_types.py
 """
 author: Sage Gendron
-
+Directs incomplete part numbers to the correct functions based on their location in the package (and in the smart
+drawing code schema).
 """
+from connections import typ_system_type, ctrl_conn_type, supply_conn_type, return_conn_type
 
 
 def type_two_kits(dwg_split, pn, size, sys_type, conn_size, conn_type, control_size_type, rate, i, f_size, f_type):
     """
-    Builds type two kit component part numbers with correct end connections and sizes matching given information.
+    Generates a package quote for type two kits.
+    Only works through one component at a time as indicated by the 'i' iterative variable.
 
     :param list dwg_split: smart kit code (drawing name) split by hyphens
-    :param str pn: current base part number to be modified based on location in dwg code and sizes referenced
-    :param str size: system size value (in letter form) from cell in this particular row from the schedule
-    :param str sys_type: system type value from cell in this particular row from the schedule
-    :param str conn_size: connection size value (in letter form) from cell in this particular row from the schedule
-    :param str conn_type: connection type value from cell in this particular row from the schedule
-    :param str control_size_type: control size/type value from cell in this particular row from the schedule
-    :param float rate: rate value from cell in this particular row from the schedule
+    :param str pn: part number to be modified based on location in dwg code and sizes referenced
+    :param str size: system size value (in letter form)
+    :param str sys_type: system type value
+    :param str conn_size: connection size value (in letter form)
+    :param str conn_type: connection type value
+    :param str control_size_type: control size/type value
+    :param float rate: engineer-provided rate
     :param int i: iterated index of current letter in drawing code
-    :param f_size: f component size (only if needed to be specified in special cases ie not matching system size)
-    :param f_type: f component type (only if needed to be specified so far)
+    :param str f_size: f component size (only if needed to be specified in special cases (ie not matching system size))
+    :param str f_type: f component type (only if needed to be specified)
     :return:
-        - pn - current base part number to be modified based on location in dwg code and sizes referenced
-        - f_size - f component size (only if needed to be specified in special cases ie not matching system size)
-        - f_type - f component type (only if needed to be specified so far)
-    :rtype: (str, , )
+        - pn - completed part number
+        - f_size - f component size (only if needed to be specified in special cases (ie not matching system size))
+        - f_type - f component type (only if needed to be specified)
+    :rtype: (str, str, str)
     """
     if i == 1:
-        # check runout hoses in case SWT pipe type forces components to be THD to accommodate hose
+        # check for system-side f components in case certain types force components to be female
         if '=RF' in dwg_split[1]:
             pn = pn.replace('+', 'F', 1)
         else:
             pn = typ_system_type(pn, sys_type)
-
-        # check if f component in package to force FNPT
+        # check if f component in package to force a female connection
         pn = supply_conn_type(pn, size, conn_size, conn_type, 'F' in dwg_split[1][:4])
+
     elif i == 2:
-        # check if f component in package to force FNPT
+        # check if f component in package to force a female connection
         pn, f_size, f_type = return_conn_type(pn, size, conn_size, conn_type, rate, f_size, f_type,
                                               'F' in dwg_split[1][:4])
         pn = ctrl_conn_type(pn, size, control_size_type, conn_size, 'F' in dwg_split[1][:4])
+
     elif i == 3:
         pn = typ_system_type(pn, sys_type)
-
-        # check for supply control to alter connection type
-        if '=SCV' in dwg_split[1]:
+        # check for supply control to alter connection type function called
+        if '=SC' in dwg_split[1]:
             pn = supply_conn_type(pn, size, conn_size, conn_type)
         else:
-            # check if f component in package to force FNPT
+            # check if f component in package to force a female connection
             pn = ctrl_conn_type(pn, size, control_size_type, conn_size, 'F' in dwg_split[1][:4])
 
     return pn, f_size, f_type
@@ -57,52 +58,56 @@ def type_two_kits(dwg_split, pn, size, sys_type, conn_size, conn_type, control_s
 
 def type_three_kits(dwg_split, pn, size, sys_type, conn_size, conn_type, control_size_type, rate, i, f_size, f_type):
     """
-    Builds type three kit component part number with correct end connections and sizes matching given information.
+    Generates a package quote for type three kits.
+    Only works through one component at a time as indicated by the 'i' iterative variable.
 
     :param list dwg_split: smart kit code (drawing name) split by hyphens
     :param str pn: current base part number to be modified based on location in dwg code and sizes referenced
-    :param str size: system size value (in letter form) from cell in this particular row from the schedule
-    :param str sys_type: system type value from cell in this particular row from the schedule
-    :param str conn_size: connection size value (in letter form) from cell in this particular row from the schedule
-    :param str conn_type: connection type value from cell in this particular row from the schedule
-    :param str control_size_type: control size/type value from cell in this particular row from the schedule
-    :param float rate: rate from rate cell in this particular row from the schedule
+    :param str size: system size value (in letter form)
+    :param str sys_type: system type value
+    :param str conn_size: connection size value (in letter form)
+    :param str conn_type: connection type value
+    :param str control_size_type: control size/type value
+    :param float rate: engineer-provided rate
     :param int i: iterative variable to identify index of part/location of component being quoted
-    :param f_size: f component size (only if needed to be specified in special cases ie not matching runout size)
-    :param f_type: f component type (only if needed to be specified so far)
+    :param str f_size: f component size (only if needed to be specified in special cases ie not matching system size)
+    :param str f_type: f component type (only if needed to be specified so far)
     :return:
-        - pn - current base part number to be modified based on location in dwg code and sizes referenced
-        - hose_size - hose size (only if needed to be specified in special cases ie not matching runout size)
-        - hose_type - hose type (only if needed to be specified so far)
-    :rtype: (str, , )
+        - pn - completed part number
+        - f_size - f component size (only if needed to be specified in special cases (ie not matching system size))
+        - f_type - f component type (only if needed to be specified so far)
+    :rtype: (str, str, str)
     """
     if i == 1:
-        # check runout hoses in case SWT pipe type forces components to be THD to accommodate hose
+        # check for system-side f components in case certain types force components to be female
         if '=RF' in dwg_split[1]:
             pn = pn.replace('+', 'F', 1)
         else:
             pn = typ_system_type(pn, sys_type)
-        # check for supply control valve to alter tailpiece type
-        if '=SCV' in dwg_split[1]:
+        # check for supply control to alter connection type function called
+        if '=SC' in dwg_split[1]:
             pn = ctrl_conn_type(pn, size, control_size_type, conn_size)
         else:
             pn = supply_conn_type(pn, size, conn_size, conn_type, 'F' in dwg_split[1][:4])
+
     elif i == 2:
-        # check for hoses to force FNPT
+        # check for f components to force female
         pn, f_size, f_type = return_conn_type(pn, size, conn_size, conn_type, rate, f_size, f_type,
                                               'F' in dwg_split[1][:4])
         pn = ctrl_conn_type(pn, size, control_size_type, conn_size, 'F' in dwg_split[1][:4])
+
     elif i == 3:
-        # check system f components in case SWT pipe type forces components to be THD to accommodate f component
+        # check for system-side f components in case certain types force components to be female
         if '=RF' in dwg_split[1]:
             pn = pn.replace('+', 'F', 1)
         else:
             pn = typ_system_type(pn, sys_type)
-        # check for hoses to force FNPT
+        # check for f components to force female
         pn = ctrl_conn_type(pn, size, control_size_type, conn_size, 'F' in dwg_split[1][:4])
+
     elif i == 4:
         pn = typ_system_type(pn, sys_type)
-        # check for engineered component type on the type 3 kits, so it can force it to match system type
+        # check for engineered component type on the type 3 kits, so it can force it to match system type if required
         if dwg_split[0][4] == 'C':
             pn = typ_system_type(pn, sys_type)
         else:
@@ -112,54 +117,52 @@ def type_three_kits(dwg_split, pn, size, sys_type, conn_size, conn_type, control
     return pn, f_size, f_type
 
 
-def no_control_kits(dwg_split, pn, size, sys_type, conn_size, conn_type, rate, i,
-                    f_size, f_type):
+def no_control_kits(dwg_split, pn, size, sys_type, conn_size, conn_type, rate, i, f_size, f_type):
     """
-    Generates a package quote for kits without control valves. Only works through one component at a time as indicated
-    by the 'i' iterative variable taken as a parameter.
+    Generates a package quote for kits without controls.
+    Only works through one component at a time as indicated by the 'i' iterative variable.
 
     :param list dwg_split: smart kit code (drawing name) split by hyphens
-    :param str pn: previously generated base part number to be edited with correct connection types
-    :param str size: system size value (in letter form) from cell in this particular row from the schedule
-    :param str sys_type: system type value from cell in this particular row from the schedule
-    :param str conn_size: connection size value (in letter form) from cell in this particular row from the schedule
-    :param str conn_type: connection type value from cell in this particular row from the schedule
-    :param float rate: rate from rate cell in this particular row from the schedule
+    :param str pn: previously generated base part number to be edited with correct connections
+    :param str size: system size value (in letter form)
+    :param str sys_type: system type value
+    :param str conn_size: connection size value (in letter form)
+    :param str conn_type: connection type value
+    :param float rate: engineer-provided rate
     :param int i: iterative variable indicating location of currently quoted component in drawing name
-    :param f_size: indicates f component size (since it can vary based on max rates)
-    :param f_type: indicates f component type (since it can only vary between sweat and threaded)
+    :param str f_size: indicates f component size (since it can vary based on max rates)
+    :param str f_type: indicates f component type (since it can only vary between threaded and not-threaded)
     :return:
-        - pn - current base part number to be modified based on location in dwg code and sizes referenced
-        - f_size - f component size (only if needed to be specified in special cases ie not matching runout size)
+        - pn - completed part number
+        - f_size - f component size (only if needed to be specified in special cases (ie not matching system size))
         - f_type - f component type (only if needed to be specified so far)
-    :rtype: (str, , )
+    :rtype: (str, str, str)
     """
-    # if first component in kit
     if i == 0:
         if len(dwg_split) > 1:
-            # check system f components in case SWT pipe type forces components to be THD to accommodate f components
+            # check for system-side f components in case certain types force components to be female
             if '=RF' in dwg_split[1]:
                 pn = pn.replace('+', 'F', 1)
             else:
                 pn = typ_system_type(pn, sys_type)
-            # check if f components in package to force FNPT to accommodate f components
+            # check if f components in package to force female to accommodate f components
             pn = supply_conn_type(pn, size, conn_size, conn_type, 'F' in dwg_split[1][:4])
         elif len(dwg_split) == 1:
             pn = typ_system_type(pn, sys_type)
             pn = supply_conn_type(pn, size, conn_size, conn_type)
-    # if second component in kit
+
     elif i == 1:
         pn, f_size, f_type = return_conn_type(pn, size, conn_size, conn_type, rate, f_size, f_type,
                                               'F' in dwg_split[1][:4])
         pn = supply_conn_type(pn, size, conn_size, conn_type)
-    # if third component in kit
+
     elif i == 2:
-        # check system f components in case SWT pipe type forces components to be FNPT to accommodate f components
+        # check for system-side f components in case certain types force components to be female
         if '=RF' in dwg_split[1]:
             pn = pn.replace('+', 'F', 1)
         else:
             pn = typ_system_type(pn, sys_type)
-        # check if f component in package to force FNPT to accommodate f components
+        # check if f component in package to force female to accommodate f components
         pn = supply_conn_type(pn, size, conn_size, conn_type, 'F' in dwg_split[1][:4])
 
     return pn, f_size, f_type
